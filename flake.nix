6886833a09f7affa2621665ee01ca91d41fb3542
Franck Rasolo <franckrasolo@users.noreply.github.com>
2025-11-21 21:57:09 +0000

    use the latest release of flake-checker due to breaking changes

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
 
           shellHook = ''
             # health checks for Nix flake inputs
-            nix run "github:DeterminateSystems/flake-checker"
+            nix run "github:DeterminateSystems/flake-checker/v0.2.8"
 
             export MACOSX_DEPLOYMENT_TARGET="15.0"
           '';
